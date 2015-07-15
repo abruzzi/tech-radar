9562861854ebb25ae230c3204d7485a46471cbd8
Qiu Juntao <juntao.qiu@gmail.com>
2015-07-15 17:12:53 +1000

    add bower.json

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ $(function () {
     $.getJSON('data/t-hack-day-skillsets.json', function(data) {
         ['platform', 'technique', 'tools', 'language', 'framework'].forEach(function(category) {
             generateChart(data, category);
-            generateBubble({children: flatten(data)});
         });
+        generateBubble({children: flatten(data)});
     });
 });
